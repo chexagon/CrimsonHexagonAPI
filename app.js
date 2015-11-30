c3a620414ea8f9c17e9889ba34447b5223daa775
johntanza <jtanza@crimsonhexagon.com>
2015-11-30 11:43:19 -0500

    add local files

---
+++ app.js
@@ -0,0 +1,350 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var request = require('request');
+
+//update with user generated auth token
+var auth = 'tPfAG7dwHWS_Aj-Xz0Nm2VJ5c2FA2Lk8U_pi5H5mIYA';
+
+
+/****************************************************************                                          
+ * functions to reference date parameters in code throughout file*
+ ****************************************************************/
+
+var date = new Date();
+var endMonth  = date.getMonth();
+var endDay = date.getDate();
+var endYear = date.getFullYear();
+
+//sets end date parameter for calls to API
+//end date in all calls is 7 days from the current date
+var endDate = endYear + '-' + endMonth + '-' + endDay;
+
+var d = new Date();
+d.setDate(d.getDate() - 7);
+var startMonth = d.getMonth();
+var startDay = d.getDate();
+var startYear = d.getFullYear();
+
+//sets start date parameter for calls to API 
+//start date is set to most current date
+var startDate = startYear + '-' + startMonth + '-' + startDay;
+
+
+//ensures that start and end date is formatted properly as API  calls require
+function fixEndMonth() {
+    endMonth = endMonth + 1;
+    if (endMonth < 10) {
+        endMonth = "0" + endMonth;
+    } else {
+        return endMonth;
+    }
+}
+
+function fixEndDay() {
+    if (endDay < 10) {
+        endDay = "0" + endDay;
+    } else {
+        return endDay;
+    }
+}
+
+
+function fixStartMonth() {
+    startMonth = startMonth + 1;
+    if (startMonth < 10) {
+        startMonth = "0" + startMonth;
+    } else {
+        return startMonth;
+    }
+}
+
+function fixStartDay() {
+    if (startDay < 10) {
+        startDay = "0" + startDay;
+    } else {
+        return startDay;
+    }
+}
+
+
+fixEndMonth();
+fixEndDay(); 
+fixStartDay();
+fixStartMonth();
+
+
+
+
+/**************************************                                    
+ * Collects gender data from monitor  *
+ **************************************/
+
+var genderCollect = function(callback) {
+
+  var monitorId = "2086803019";
+  var genderUrl = ('https://forsight.crimsonhexagon.com/api/monitor/demographics/gender?id=' + monitorId + '&start=' + startDate + '&end=' + endDate + '&auth=' + auth);
+
+
+
+  var male = [];
+  var female = [];
+  var gender = {};
+
+
+  request({
+    url: genderUrl,
+    method: 'GET'
+  }, function(err, response, body) {
+    if (!err && response.statusCode === 200) {
+      var data = JSON.parse(body);
+      for (var i = 0; i < data.genderCounts.length; i++) {
+        male.push(data.genderCounts[i].genderCounts.percentFemale);
+        female.push(data.genderCounts[i].genderCounts.percentMale);
+      }
+      gender = [male, female];
+    }
+    callback(gender);
+  });
+}
+
+//sends data to client on connection
+function logArray(gender) {
+  io.on('connection', function(socket) {
+    socket.emit('gender', gender);
+  });
+}
+
+genderCollect(logArray);
+
+
+/******************************************************                                                    
+ * collects total activity metrics from facebook SAMs *
+ ******************************************************/                                                   
+
+var fbCollect = function(callback) {
+
+  var monitorId = "2088171268";
+  var facebookUrl = ('https://forsight.crimsonhexagon.com/api/monitor/facebook/totalactivity?id=' + monitorId + '&start=' + startDate + '&end=' + endDate + '&auth=' + auth);
+
+
+  var likes = [];
+  var comments = [];
+  var shares = [];
+  var totalActivity = {};
+
+  request({
+    url: facebookUrl,
+    method: 'GET'
+  }, function(err, response, body) {
+    if (!err && response.statusCode === 200) {
+      var data = JSON.parse(body);
+      for (var i = 0; i < data.dailyResults.length; i++) {
+        likes.push(data.dailyResults[i].admin.likesOnAdmin);
+        comments.push(data.dailyResults[i].admin.commentsOnAdmin);
+        shares.push(data.dailyResults[i].admin.sharesOnAdmin);
+      }
+      totalActivity = [likes, comments, shares];
+    }
+    callback(totalActivity);
+  });
+}
+
+
+//sends data to client on connection
+function logFb(totalActivity) {
+  io.on('connection', function(socket) {
+    socket.emit('totalActivity', totalActivity);
+  });
+}
+
+fbCollect(logFb);
+
+
+/**************************************************                                                
+ * function to collect data from results endpoint *
+ **************************************************/
+
+var monitorResults = function(callback) {
+
+  var monitorId = "2086803019";
+  var resultsUrl = ('https://api.crimsonhexagon.com/api/monitor/results?id=' + monitorId + '&start=' + startDate + '&end=' + endDate + '&auth=' + auth);
+
+  var neutralProportion = [];
+  var positiveProportion = [];
+  var negativeProportion = [];
+  var resultsData = {};
+
+
+  request({
+    url: resultsUrl,
+    method: 'GET'
+  }, function(err, response, body) {
+    if (!err && response.statusCode === 200) {
+      var data = JSON.parse(body);
+      for (var i = 0; i < data.results.length; i++) {
+        neutralProportion.push(data.results[i].categories[0].proportion);
+        positiveProportion.push(data.results[i].categories[1].proportion);
+        negativeProportion.push(data.results[i].categories[2].proportion);
+      }
+      resultsData = [neutralProportion, positiveProportion, negativeProportion]
+    }
+    callback(resultsData);
+  });
+}
+
+//sends data to client on connection
+function sendResultsData(resultsData) {
+  io.on('connection', function(socket) {
+    socket.emit('resultsChart', resultsData);
+  });
+}
+
+monitorResults(sendResultsData);
+
+
+/**************************************
+ * function to collect volume metrics * 
+ **************************************/
+
+var twitterVolume = function(callback) {
+
+  var monitorId = "2086803019";
+  var volumeUrl = ('https://forsight.crimsonhexagon.com/api/monitor/dayandtime?id=' + monitorId + '&start=' + startDate + '&end=' + endDate + '&auth=' + auth);
+
+  var weeklyVolume = [];
+  var weeklyObject = {};
+
+  request({
+    url: volumeUrl,
+    method: 'GET'
+  }, function(err, response, body) {
+    var weeklyVolume = [];
+    if (!err && response.statusCode === 200) {
+      var data = JSON.parse(body);
+      for (var i = 0; i < data.volumes.length; i++) {
+        weeklyVolume.push(data.volumes[i].numberOfDocuments);
+      }
+      weeklyObject = weeklyVolume;
+    }
+    callback(weeklyObject);
+  });
+}
+
+
+//sends data to client on connection
+function sendVolume(weeklyObject) {
+  io.on('connection', function(socket) {
+    socket.emit('weeklyVolume', weeklyObject);
+  });
+}
+
+twitterVolume(sendVolume);
+
+
+/****************************************                                      
+ * function to gather instagram metrics *
+ ****************************************/
+
+var instagramActivity = function(callback) {
+
+    var monitorId = "2086809591";
+    var instagramUrl = ('https://forsight.crimsonhexagon.com/api/monitor/instagram/totalactivity?id=' + monitorId + '&start=' + startDate + '&end=' + endDate + '&auth=' + auth);
+
+
+      var instaLikes = [];
+      var instaComments = [];
+      var instaData = {};
+
+      request({
+        url: instagramUrl,
+        method: 'GET'
+      }, function(err, response, body) {
+        if (!err && response.statusCode === 200) {
+          var data = JSON.parse(body);
+          var instaLikes = [];
+          var instaComments = [];
+          for (var i = 0; i < data.dailyResults.length; i++) {
+            instaLikes.push(data.dailyResults[i].admin.likesOnAdmin);
+            instaComments.push(data.dailyResults[i].admin.commentsOnAdmin);
+          }
+          instaData = [instaLikes, instaComments];
+        }
+        callback(instaData);
+      });
+    }
+
+
+    function sendInsta(instaData) {
+      io.on('connection', function(socket) {
+        socket.emit('instaMetrics', instaData);
+      });
+    }
+
+    instagramActivity(sendInsta);
+
+
+/************************************************************                                                          
+ * function to collect total enagement metrics from twitter *
+ ************************************************************/                                            
+
+var twitterResults = function(callback) {
+
+
+  var monitorId = "2086809677";
+  var twitterUrl = ('https://forsight.crimsonhexagon.com/api/monitor/twittersocial/totalengagement?id=' + monitorId + '&start=' + startDate + '&end=' + endDate + '&auth=' + auth);
+
+
+
+  var tweetMentions = [];
+  var tweetReplies = [];
+  var tweetRetweets = [];
+  var tweetCollection = {};
+
+  request({
+    url: twitterUrl,
+    method: 'GET'
+  }, function(err, response, body) {
+    if (!err && response.statusCode === 200) {
+      var data = JSON.parse(body);
+      for (var i = 0; i < data.dailyResults.length; i++) {
+        tweetMentions.push(data.dailyResults[i].mentions);
+        tweetReplies.push(data.dailyResults[i].replies);
+        tweetRetweets.push(data.dailyResults[i].retweets);
+      }
+      tweetCollection = [tweetMentions, tweetReplies, tweetRetweets];
+    }
+    callback(tweetCollection);
+  });
+}
+
+function sendTwitterData(tweetCollection) {
+  io.on('connection', function(socket) {
+    socket.emit('twitterData', tweetCollection);
+  });
+}
+
+
+
+twitterResults(sendTwitterData);
+
+/***********************************************************************                                                
+ * sets up necessary middleware and express methods to initialie the app *
+ ***********************************************************************/ 
+
+//serves static files from the public directory
+app.use(express.static('public'));
+
+//sends our main inde.html file
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+//launches app, begins listening on port 3000
+http.listen(3000, function(){
+  console.log('listening on port:3000');
+});
+
+
+
---
+++ public/index.js
@@ -0,0 +1,144 @@
+        var socket = io();
+
+        //gender chart
+        socket.on('gender', function(data) {
+          console.log(data);
+
+          //code to run c3 charts
+          var chart = c3.generate({
+            bindto: '#chart',
+            size: {
+              height: 250,
+              width: 580
+            },
+            data: {
+        columns: [
+            ['Male'].concat(data[0]),
+            ['Female'].concat(data[1]),
+        ],
+        type : 'donut',
+            },
+          });
+        });
+
+
+ //total activity chart
+ socket.on('totalActivity', function(data) {
+   console.log(data);
+
+   //code to run c3 charts
+   var chart = c3.generate({
+     bindto: '#chart2',
+     data: {
+       x: 'x',
+       columns: [
+         ['x', '2010-01-01', '2011-01-01', '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01'],
+         ['Likes'].concat(data[0]), ['Comments'].concat(data[1]), ['Shares', ].concat(data[2]),
+       ],
+       type: 'bar',
+     },
+     axis: {
+       x: {
+         type: 'timeseries',
+       }
+     }
+   });
+ }); 
+          
+
+        //sentiment results chart
+        socket.on('resultsChart', function(data) {
+          console.log(data);
+
+          //code to run c3 chart
+          var chart = c3.generate({
+            bindto: '#chart3',
+
+            data: {
+              columns: [
+                ['Neutral'].concat(data[0]), ['Positive'].concat(data[1]), ['Negative'].concat(data[2]),
+              ],
+              type: 'pie',
+            }
+          });
+        });
+
+
+        //volume chart
+        socket.on('weeklyVolume', function(data) {
+          console.log(data);
+
+          var chart = c3.generate({
+            bindto: '#chart4',
+    data: {
+      x: 'x',
+        columns: [
+            ['x', '2010-01-01', '2011-01-01', '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01'],
+            ['Volume'].concat(data),
+        ],
+        types: {
+            Volume: 'area-spline'
+        }
+    },
+    axis: {
+       x: {
+         type: 'timeseries',
+       }
+     }
+});
+        });
+
+
+        //twitter metrics chart
+        socket.on('twitterData', function(data) {
+          console.log(data);
+
+          //c3 chart data
+          var chart = c3.generate({
+            bindto:'#chart6',
+    data: {
+      x: 'x',
+        columns: [
+            ['x', '2010-01-01', '2011-01-01', '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01'],
+            ['Replies'].concat(data[1]),
+            ['Retweets'].concat(data[2]),
+        ],
+        types: {
+            Replies: 'area-spline',
+            Retweets: 'area-spline'
+            // 'line', 'spline', 'step', 'area', 'area-step' are also available to stack
+        },
+        groups: [['Replies', 'Retweets']]
+    },
+    axis: {
+       x: {
+         type: 'timeseries',
+       }
+     }
+});
+        });
+
+        //instagram metircs chart
+        socket.on('instaMetrics', function(data) {
+          console.log(data);
+
+          //c3 chart data
+          var chart = c3.generate({
+            bindto:'#chart5',
+    data: {
+      x: 'x',
+        columns: [
+            ['x', '2010-01-01', '2011-01-01', '2012-01-01', '2013-01-01', '2014-01-01', '2015-01-01'],
+            ['Likes'].concat(data[0]),
+            ['Comments'].concat(data[1]),
+        ]
+    },
+    axis: {
+       x: {
+         type: 'timeseries',
+       }
+     }
+});
+        });
+
+
